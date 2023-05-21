4c73f85b80525c9356ac3b6911545713a3365b7f
Alexander Bogachenkov <sashako4etov@yandex.ru>
2023-05-22 00:13:08 +0300

    sprint 15 commit 1.2

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = "alexboga.projectback.nomoredomains.monster";
+const BASE_URL = "https://alexboga.projectback.nomoredomains.monster";
 // const BASE_URL = "http://127.0.0.1:3000"; // тестируем локально
 
 const _checkResponse = (res) => {
